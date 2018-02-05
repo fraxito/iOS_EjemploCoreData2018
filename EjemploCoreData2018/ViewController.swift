f222da0003c01cd541a9bc28861bb2be7941d353
Marta Amengual <marta.amengual@hotmail.com>
2018-02-05 22:19:34 +0100

    Initial Commit

---
+++ EjemploCoreData2018/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EjemploCoreData2018
+//
+//  Created by Jorge Cisneros on 5/2/18.
+//  Copyright © 2018 Jorge Cisneros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
