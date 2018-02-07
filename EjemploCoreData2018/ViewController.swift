d87caf3a353b20d72642572e184b7086f61d1c13
Marta Amengual <marta.amengual@hotmail.com>
2018-02-07 17:44:25 +0100

    Funciona todo:  - se inserta correctamente  - la navegación es correcta también  - código optimizado para Swift 3

--- EjemploCoreData2018/ViewController.swift
+++ EjemploCoreData2018/ViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBAction func cancelar(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+
+    @IBAction func guardar(_ sender: Any) {
+        let _nombreMascota = self.nombreMascota.text!
+
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let entidad = NSEntityDescription.entity(forEntityName: "Mascota", in: managedContext)
+        
+        let registro = NSManagedObject(entity: entidad!, insertInto: managedContext)
+        
+        registro.setValue(_nombreMascota, forKey: "nombre")
+        
+        do{
+            try managedContext.save()
+            print("gato guardado ok")
+        }   catch let error as NSError{
+            print("No se ha podido escribir el gato \(error), \(error.userInfo)")
+        }
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    @IBOutlet weak var nombreMascota: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        
     }
 
     override func didReceiveMemoryWarning() {
